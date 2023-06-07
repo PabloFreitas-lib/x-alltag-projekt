fe4cb4932e7f8fe8cbeaeca9492b109be37fa920
X-Alltag-Rechner <x-alltag@proton.me>
2023-06-07 12:41:24 +0200

    Erste SocketFiles

---
+++ xallt/Assets/Scripts/Mindmapping/File.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class File : MonoBehaviour
+{
+    public string name;
+    // Start is called before the first frame update
+    
+}
---
+++ xallt/Assets/Scripts/Mindmapping/FileSocket.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class FileSocket : MonoBehaviour
+{
+    public File socketFile;
+    public Text fileNameText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("File"))
+        {
+            socketFile = other.GetComponent<File>();
+            fileNameText.text = socketFile.name;
+            Debug.Log(socketFile.name);
+        }
+    }
+
+}

2c1af97364e99a149736f4f8e99852378e0535cc
Christoph Dreier <dreierc@uni-bremen.de>
2023-06-07 21:26:30 +0200

    Update LightController.cs
    Ambient light control and on/off switch

--- xallt/Assets/Lights/LightController.cs
+++ xallt/Assets/Lights/LightController.cs
@@ -8,14 +8,22 @@ public class LightController : MonoBehaviour
     private CustomLightScript[] lights;
 
     public bool useTemperatureControl = true; // Toggle between temperature and RGB control
+    public bool lightonoff = true;  //lightswitch
     public float brightness = 1f;
-    public Color color = Color.white;
-    public float temperature = 6500f;
+    public Color color = Color.HSVToRGB(60f / 360f, 1f, 1f);
+    public float temperature;
+
+
+    // Slider for light temperature / color change
+    [Range(0f,1f)]
+    public float tempSlider;
 
     private void Start()
     {
         // Find all light components with the CustomLightScript attached
         lights = FindObjectsOfType<CustomLightScript>();
+        // Startingpoint in Slider
+        tempSlider = 0f;
     }
 
     public void SetBrightness(float brightness)
@@ -47,17 +55,50 @@ public class LightController : MonoBehaviour
         // Update the properties of all lights every frame
         foreach (CustomLightScript light in lights)
         {
+
             if (useTemperatureControl)
             {
-                light.SetColor(Color.white);
+                Color color = Color.HSVToRGB(45f / 360f, tempSlider, 1f); // light temperature
+                light.SetColor(color);
                 light.SetTemperature(temperature);
                 light.SetBrightness(brightness);
             }
             else
             {
+                Color color = Color.HSVToRGB(tempSlider, 1f, 1f); // color change 
                 light.SetTemperature(0);
                 light.SetColor(color);
                 light.SetBrightness(brightness);
+            }
+
+            if (lightonoff)
+            {
+                //lights on
+                if (useTemperatureControl)
+                {
+                    Color color = Color.HSVToRGB(45f / 360f, tempSlider, 1f);
+                    light.SetColor(color);
+                }
+                else
+                {
+                    Color color = Color.HSVToRGB(tempSlider, 1f, 1f);
+                    light.SetColor(color);
+                }
+            }
+            else
+            {
+                //lights off
+                if (useTemperatureControl)
+                {
+                    Color color = Color.HSVToRGB(45f / 360f, tempSlider, 0f); // light temperature
+                    light.SetColor(color);
+                }
+                else
+                {
+                    Color color = Color.HSVToRGB(tempSlider, 1f, 0f);
+                    light.SetColor(color);
+                }
+
             }
         }
     }

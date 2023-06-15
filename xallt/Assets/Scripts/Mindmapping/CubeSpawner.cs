6ac5fdd953cd5ea8a2c78db2b871b8ee3cc5a36a
Dmitry Tschernobai <dmitry@uni-bremen.de>
2023-06-15 13:30:40 +0200

    MülleimerFunktion hinzugefügt

--- xallt/Assets/Scripts/Mindmapping/CubeSpawner.cs
+++ xallt/Assets/Scripts/Mindmapping/CubeSpawner.cs
@@ -5,44 +5,31 @@ using UnityEngine.InputSystem.XR;
 
 public class CubeSpawner : MonoBehaviour
 {
-    public Transform rightHand;
-    public InputActionReference pinchAction; // Reference to the Trigger input action
-    public GameObject cubePrefab; // Prefab of the cube to spawn
-
-    private bool isTriggerPressed = false;
-
-    private void Start()
-    {
-        // Enable the trigger action
-        pinchAction.action.Enable();
-    }
+    // Reference to the Trigger input action
+    public GameObject filePrefab; // Prefab of the cube to spawn
+    public bool isSpawned;
+    Collider detectionCollider;
 
     private void Update()
     {
-        // Check if the trigger button is pressed
-        if (pinchAction.action.triggered && !isTriggerPressed)
+        Collider[] colliders = Physics.OverlapBox(detectionCollider.bounds.center, detectionCollider.bounds.extents, detectionCollider.transform.rotation);
+
+        if (colliders.Length == 0)
         {
-            isTriggerPressed = true;
             SpawnCube();
         }
-        else if (!pinchAction.action.triggered && isTriggerPressed)
-        {
-            isTriggerPressed = false;
-        }
+    }
+
+    private void Start()
+    {
+        SpawnCube();
     }
 
     private void SpawnCube()
     {
         // Instantiate a cube prefab at the position of the hand controller
-        GameObject cube = Instantiate(cubePrefab, rightHand.position, Quaternion.identity);
-        // Parent the cube to the hand controller
-        cube.transform.SetParent(transform);
-    }
-
-    private void OnDisable()
-    {
-        // Disable the trigger action
-        pinchAction.action.Disable();
+        Instantiate(filePrefab, transform.position, Quaternion.identity);
+        isSpawned = true;
     }
 }
 

7e6af2f4080300aeef603a1b11be7a501b57c868
Jie Su <sujie@uni-bremen.de>
2023-06-21 19:08:16 +0200

    Fixed Errors von SaveDrawingAsImage()

--- xallt/Assets/Scripts/Whiteboard.cs
+++ xallt/Assets/Scripts/Whiteboard.cs
@@ -3,25 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+
 [System.Serializable]
 public class Whiteboard : MonoBehaviour
 {
     public Texture2D texture;
     public Vector2 textureSize = new Vector2(2048, 2048);
-    
+    private RenderTexture targetTexture;
 
-    [SerializeField]private string m_ID = Guid.NewGuid().ToString();
-    
+    [SerializeField] private string m_ID = Guid.NewGuid().ToString();
+
     //Getter Method
     public string id => m_ID;
+
+    public RenderTexture TargetTexture { get; internal set; }
 
     [ContextMenu("Generate new ID")]
     private void RegenerateGUID() => m_ID = Guid.NewGuid().ToString();
 
     public Whiteboard(string idargs)
     {
-      m_ID = idargs;
-    } 
+        m_ID = idargs;
+    }
 
     void Start()
     {
@@ -31,3 +34,5 @@ public class Whiteboard : MonoBehaviour
     }
 
 }
+
+

84fafab19b45175f182d8cd93e9ec94f90f9917d
PabloFreitas-lib <pablofreitas.fn@gmail.com>
2023-06-22 15:26:51 +0200

    [Core-Features] - Basic example from from animation + interaction using the window. Template for future animation and interactions combinations Participants: Sophia Pablo Fabian

---
+++ xallt/Assets/TriggerAnimation.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TriggerAnimation : MonoBehaviour
+{
+    public XRBaseInteractable interactable; // The window with XR Grab Interactable
+    public Animator windowAnimator; // The Animator of the window
+
+    void OnEnable()
+    {
+        // debug log to see if is working
+        Debug.Log("TriggerAnimation script is enabled");
+        interactable.onSelectEntered.AddListener(StartTurn);
+        interactable.onSelectExited.AddListener(StopTurn);
+    }
+
+    void OnDisable()
+    {
+        // debug log to see if is working
+        Debug.Log("TriggerAnimation script is disabled");
+        interactable.onSelectEntered.RemoveListener(StartTurn);
+        interactable.onSelectExited.RemoveListener(StopTurn);
+    }
+
+    private void StartTurn(XRBaseInteractor interactor)
+    {
+        windowAnimator.SetBool("isTurning", true);
+    }
+
+    private void StopTurn(XRBaseInteractor interactor)
+    {
+        windowAnimator.SetBool("isTurning", false);
+    }
+}

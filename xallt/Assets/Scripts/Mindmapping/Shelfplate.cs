482a1acc458d81f0cb4eda7228625021be229062
Mert Kaynak <kaynak@uni-bremen.de>
2023-06-29 20:23:25 +0200

    Regal mit Funktionen
    Mert Kaynak

--- xallt/Assets/Scripts/Mindmapping/Shelfplate.cs
+++ xallt/Assets/Scripts/Mindmapping/Shelfplate.cs
@@ -3,21 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
-public class FileSocket : MonoBehaviour
+public class Shelfplate : MonoBehaviour
 {
     public File socketFile;
     public Text fileNameText;
+
+    public List<File> filelist;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -25,7 +27,7 @@ public class FileSocket : MonoBehaviour
         {
             socketFile = other.GetComponent<File>();
             fileNameText.text = socketFile.fileName;
-            socketFile.isOpen = true;
+            filelist.Add(socketFile);
             Debug.Log(socketFile.fileName);
         }
     }
@@ -33,10 +35,9 @@ public class FileSocket : MonoBehaviour
     {
         if (other.CompareTag("File"))
         {
+            filelist.Remove(socketFile);
             fileNameText.text = "";
             socketFile = other.GetComponent<File>();
-            socketFile.isOpen = false;
         }
     }
-
 }

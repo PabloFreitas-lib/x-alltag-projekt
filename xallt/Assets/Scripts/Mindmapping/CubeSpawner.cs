344c4700eccead3571f91cdbd355663e7a74c033
Mert Kaynak <kaynak@uni-bremen.de>
2023-07-06 20:17:55 +0200

    CubeSpawner auf UI funktioniert wie gewünscht
    Wenn man auf den Cubespawn-Button klickt, spawnt der Cube auf der UI und wartet darauf, aus der UI entnommen zu werden.
    
    - Mert Kaynak

--- xallt/Assets/Scripts/Mindmapping/CubeSpawner.cs
+++ xallt/Assets/Scripts/Mindmapping/CubeSpawner.cs
@@ -11,6 +11,8 @@ public class CubeSpawner : MonoBehaviour
     public GameObject mindmap;
     public Transform mindMapSpawnPosition;
 
+    private GameObject cube;
+
     private void Update()
     {
        Collider[] colliders = Physics.OverlapBox(detectionCollider.bounds.center, detectionCollider.bounds.extents, detectionCollider.transform.rotation);
@@ -19,6 +21,13 @@ public class CubeSpawner : MonoBehaviour
         {
             SpawnCube(transform);
         }
+
+        if (cube != null)
+            if (cube.transform.childCount == 2 && cube.GetComponent<Rigidbody>().isKinematic)
+            {
+                cube.transform.parent = null;
+                cube.GetComponent<Rigidbody>().isKinematic = false;
+            }
     }
 
     private void Start()
@@ -33,6 +42,17 @@ public class CubeSpawner : MonoBehaviour
         GameObject map = Instantiate(mindmap,mindMapSpawnPosition.position, Quaternion.identity);
         cube.GetComponent<File>().map = map.GetComponent<Mindmap>();
         isSpawned = true;
+    }
+
+    public void SpawnCubeUI(Transform spawnPosition)
+    {
+        // Instantiate a cube prefab at the position of the hand controller
+        this.cube = Instantiate(filePrefab, spawnPosition.position, Quaternion.identity);
+        GameObject map = Instantiate(mindmap, mindMapSpawnPosition.position, Quaternion.identity);
+        cube.GetComponent<File>().map = map.GetComponent<Mindmap>();
+        isSpawned = true;
+        cube.GetComponent<Rigidbody>().isKinematic = true;
+        cube.transform.parent = spawnPosition;
     }
 }
 

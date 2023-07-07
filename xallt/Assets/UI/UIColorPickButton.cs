8cda7c16de9cc9de66d298e5055b33e37e00afa7
Desktop-MZH-3240 <Desktop-MZH-3240>
2023-07-07 13:13:48 +0200

    Script Colorpicker

--- xallt/Assets/UI/UIColorPickButton.cs
+++ xallt/Assets/UI/UIColorPickButton.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-
+using UnityEngine.XR.Interaction.Toolkit;
 
 namespace InfoGamerHubAssets
 {
@@ -17,10 +17,15 @@ namespace InfoGamerHubAssets
         public RectTransform colorChartRect;
 
         public RectTransform cursor;
+        public XRRayInteractor rayinteractor;
+
 
         public Color newcolor;
         [SerializeField] Image button;
         [SerializeField] Image cursorColor;
+
+        public int xcol = 0;
+        public int ycol = 0;
 
         public void PickColor(BaseEventData data)
         {
@@ -28,12 +33,18 @@ namespace InfoGamerHubAssets
 
             cursor.position = pointer.position;
 
-            Vector2 cursorRealPosition = new Vector2(colorChartRect.rect.width / 2 + cursor.localPosition.x, colorChartRect.rect.height / 2 + cursor.localPosition.y);
+            rayinteractor.TryGetCurrent3DRaycastHit(out RaycastHit raycastHit);
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(colorChartRect, raycastHit.point, Camera.main, out Vector2 localPoint);
+            Debug.Log(localPoint.x);
+            Debug.Log(localPoint.y);
+            //Vector2 cursorRealPosition = new Vector2(colorChartRect.rect.width / 2 + cursor.localPosition.x, colorChartRect.rect.height / 2 + cursor.localPosition.y);
 
-            Color pickedColor = colorChart.GetPixel((int)(cursorRealPosition.x * (colorChart.width / colorChartRect.rect.width)), (int)(cursorRealPosition.y * (colorChart.height / colorChartRect.rect.height)));
+            //Color pickedColor = colorChart.GetPixel((int)(cursorRealPosition.x * (colorChart.width / colorChartRect.rect.width)), (int)(cursorRealPosition.y * (colorChart.height / colorChartRect.rect.height)));
+            
+            Color pickedColor = colorChart.GetPixel((int) (localPoint.x * (colorChart.width / colorChartRect.rect.width)), (int)( localPoint.y * (colorChart.height / colorChartRect.rect.height)));
             newcolor = pickedColor;
 
-            Debug.Log(pickedColor);
+            //Debug.Log(pickedColor);
             button.color = pickedColor;
             cursorColor.color = pickedColor;
             ColorPickerEvent.Invoke(pickedColor);

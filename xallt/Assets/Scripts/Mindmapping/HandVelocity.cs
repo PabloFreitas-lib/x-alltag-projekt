e006f891675499eefca28b6f3fa434c603831762
Dmitry <65425228+xXDIMA-BOSSXx@users.noreply.github.com>
2023-07-07 16:40:35 +0200

    Update HandVelocity.cs

--- xallt/Assets/Scripts/Mindmapping/HandVelocity.cs
+++ xallt/Assets/Scripts/Mindmapping/HandVelocity.cs
@@ -10,6 +10,9 @@ public class HandVelocity : XRRayInteractor
     public float deadZone = 0.2f;
     public float sensitivity = 10;
 
+    public float pullDistance = 25;
+    public float pushDistance = 60;
+
     private Rigidbody rb;
     private Vector3 previousPosition;
     public float normalizedSpeed;
@@ -28,18 +31,31 @@ public class HandVelocity : XRRayInteractor
         previousPosition = currentPosition;
 
         Vector3 direction = (target.position - transform.position).normalized;
+        //Debug.Log(direction);
+        //Debug.Log(target.position - transform.position);
+        float distance = Vector3.Distance(transform.position, target.position);
+        distance *= 100;
         float speed = Vector3.Dot(velocity, direction);
+        //Debug.Log(speed);
         normalizedSpeed = -Mathf.Clamp(speed * sensitivity / normalizationRange, -1f, 1f);
         if(normalizedSpeed <= deadZone && normalizedSpeed > -deadZone)
         {
             normalizedSpeed = 0;
+        }
+        if(distance < pullDistance)
+        {
+            normalizedSpeed = -1;
+        }
+        else if(distance > pushDistance)
+        {
+            normalizedSpeed = 1;
         }
         TranslateAnchor(rayOriginTransform, attachTransform, normalizedSpeed);
     }
 
     private void Update()
     {
-        Debug.Log(normalizedSpeed);
+        //Debug.Log(normalizedSpeed);
 
     }
 

1f66e5798346f06d1aa633a2e0ba60f202d828a1
Dmitry Tschernobai <dmitry@uni-bremen.de>
2023-08-10 15:17:39 +0200

    CanThrowingGame Start

---
+++ xallt/Assets/CanThrowingGame.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanThrowingGame : MonoBehaviour
+{
+    public BoxCollider CanCounter;
+    public int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

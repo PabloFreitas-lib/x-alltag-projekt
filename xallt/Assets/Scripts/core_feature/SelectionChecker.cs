5671ff88217548798ff466b9768e68f7eb824052
Desktop-MZH-3240 <Desktop-MZH-3240>
2023-08-14 16:56:43 +0200

    Added possibility to select object with gesture By Fabian schmurr

--- xallt/Assets/Scripts/core_feature/SelectionChecker.cs
+++ xallt/Assets/Scripts/core_feature/SelectionChecker.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Hands;
 using UnityEngine.XR.Interaction.Toolkit;
 
 
@@ -28,14 +29,33 @@ public class SelectionChecker : MonoBehaviour
     [Tooltip("Right hand selection input")]
     InputActionReference rightSelected;
 
+    [SerializeField]
+    [Tooltip("Gesture that is used to select an object")]
+    private Gesture.GestureType selectGesture;
+
+    [SerializeField]
+    [Tooltip("Describes how the object an be selected")]
+    private SelectionType selectionType;
 
     [SerializeField]
     [Tooltip("Defines in which hand this object can be hold.")]
     private PossibleHand handToHold = PossibleHand.BOTH;
 
+    /// <summary>
+    /// Time a gesture needs to be held until the object gets selected
+    /// </summary>
+    private float selectionGestureTime = 1f;
+
+    private float currentSelectionTime = 0f;
     private bool m_lastSelected = false;
 
+    private Gesture lastSelectionGesture;
+
     private CoreFeatureController controller;
+
+    [SerializeField]
+    [Tooltip("Only necessary if a gesture is used to activate this object.")]
+    private GestureRecognizer _gestureRecognizer;
 
     /// <summary>
     /// Defines in which hand this object can be hold.
@@ -50,9 +70,20 @@ public class SelectionChecker : MonoBehaviour
     }
 
     /// <summary>
+    /// Enum that indicates how the object can be selected
+    /// </summary>
+    /// <author>Fabian Schmurr</author>
+    public enum SelectionType
+    {
+        GESTURE,
+        RAYCAST,
+        BOTH
+    }
+
+    /// <summary>
     /// This function is called when the object is activated by the core feature controller.
     /// </summary>
-    /// <exception cref="MissingComponentException"> if <see cref="CoreFeatureController"/> couldn't be found </exception>
+    /// <exception cref="MissingComponentException"> if <see cref="core_feature_controller"/> couldn't be found </exception>
     /// <author> Fabian Schmurr </author>
     void Start()
     {
@@ -60,6 +91,33 @@ public class SelectionChecker : MonoBehaviour
         if (controller == null)
         {
             throw new MissingComponentException("core feature controller not found in controller holder object.");
+        }
+
+        if ((selectionType == SelectionType.BOTH) || (selectionType == SelectionType.GESTURE))
+        {
+            _gestureRecognizer.OnGestureDetected.AddListener(OnGestureDetected);
+            _gestureRecognizer.OnGestureReleased.AddListener(OnGestureReleased);
+        }
+    }
+
+    private void OnGestureReleased(Gesture arg0)
+    {
+        //stop timer
+        currentSelectionTime = 0;
+        lastSelectionGesture = null;
+    }
+
+    private void OnGestureDetected(Gesture arg0)
+    {
+        //start selection timer if gesture was performed
+        if (arg0.type == selectGesture &&
+            (arg0.handedness == Handedness.Left && handToHold == PossibleHand.LEFT
+             || arg0.handedness == Handedness.Right && handToHold == PossibleHand.RIGHT
+             || (arg0.handedness == Handedness.Right || arg0.handedness == Handedness.Left)
+             && handToHold == PossibleHand.BOTH))
+        {
+            lastSelectionGesture = arg0;
+            currentSelectionTime += Time.deltaTime;
         }
     }
 
@@ -70,32 +128,67 @@ public class SelectionChecker : MonoBehaviour
     /// <author> Fabian Schmurr </author>
     void Update()
     {
-        if(handToHold == PossibleHand.NONE)
+        if (handToHold == PossibleHand.NONE)
         { return; }
+        //in case that raycast-mode is enabled
+        if (selectionType == SelectionType.RAYCAST)
+        {
+            checkForRaycastSelection();
+        }
+        else if (selectionType == SelectionType.BOTH)
+        {
+            checkForRaycastSelection();
+            checkForGestureSelection();
+        }
+        else if (selectionType == SelectionType.GESTURE)
+        {
+            checkForGestureSelection();
+        }
+    }
+
+    private void checkForGestureSelection()
+    {
+        Debug.Log("current pen timer:\t" + currentSelectionTime);
+        if (!m_lastSelected && currentSelectionTime > 0)
+        {
+            currentSelectionTime += Time.deltaTime;
+            if (currentSelectionTime > selectionGestureTime)
+            {
+                controller.activateGameObject(gameObject, lastSelectionGesture.handedness);
+                m_lastSelected = true;
+                currentSelectionTime = 0f;
+            }
+        }
+        else if (m_lastSelected)
+        {
+            m_lastSelected = false;
+        }
+    }
+
+    private void checkForRaycastSelection()
+    {
         if (m_Interactable.isSelected && !m_lastSelected)
         {
-            if (controller != null)
+            if (handToHold == PossibleHand.LEFT || handToHold == PossibleHand.BOTH)
             {
-                if(handToHold == PossibleHand.LEFT || handToHold == PossibleHand.BOTH)
+                if (leftSelected.action.WasPressedThisFrame())
                 {
-                    if (leftSelected.action.WasPressedThisFrame())
+                    if (controller.activateGameObject(gameObject, UnityEngine.XR.Hands.Handedness.Left))
                     {
-                        if (controller.activateGameObject(gameObject, UnityEngine.XR.Hands.Handedness.Left))
-                        {
-                            m_lastSelected = true;
-                        }
+                        m_lastSelected = true;
                     }
                 }
-                if(handToHold == PossibleHand.RIGHT || handToHold == PossibleHand.BOTH)
+            }
+            if (handToHold == PossibleHand.RIGHT || handToHold == PossibleHand.BOTH)
+            {
+                if (rightSelected.action.WasPressedThisFrame())
                 {
-                    if (rightSelected.action.WasPressedThisFrame())
+                    if (controller.activateGameObject(gameObject, UnityEngine.XR.Hands.Handedness.Right))
                     {
-                        if (controller.activateGameObject(gameObject, UnityEngine.XR.Hands.Handedness.Right))
-                        {
-                            m_lastSelected = true;
-                        }
+                        m_lastSelected = true;
                     }
                 }
+
             }
         }
         else if (!m_Interactable.isSelected && m_lastSelected)
